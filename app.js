32dd67b791d90d1ae96e38a8f61b5976c6afc57d
root <ammarkhalid237@gmail.com>
2021-03-12 03:23:19 +0500

    Sign Up/ Sign In APIs

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+
 var config = require('./config.js')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 // connecting with database //
@@ -29,6 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,12 @@
 var express = require('express');
 
+const User = require('../models/user');
+const authenticate = require('../authenticate');
+
 var router = express.Router();
 router.use(express.json());
+var passport = require('passport');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -45,7 +50,47 @@ router.post('/signup', async (req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
     res.json({ success: false, err: { name: 'Email Already Exists!' } });
   }
-})
+});
+
+router.post('/login', (req, res, next) => {
+	if (!req.body.username) {
+		res.send("Body doesn't contain username!");
+	}
+	if (!req.body.password) {
+		res.send("Body doesn't contain password!");
+	}
+	User.findOne({ email: req.body.username })
+		.then(u => {
+			if (u) {
+				req.body.username = u.username;
+			}
+			passport.authenticate('local', (err, user, info) => {
+				if (err) {
+					return next(err);
+				}
+				if (!user) { // if no user is returned, info would contain the reason
+					res.statusCode = 401;
+					res.setHeader('Content-Type', 'application/json');
+					res.json({ success: false, status: 'Login Failed', err: info });
+				}
+				else {
+					req.logIn(user, (err) => {
+						if (err) {
+							res.statusCode = 401;
+							res.setHeader('Content-Type', 'application/json');
+							res.json({ success: false, status: 'Login Failed', err: 'Could not log in user!' });
+						}
+						var token = authenticate.getToken({ _id: user._id });
+            var userInfo = { username: user.username, id: user._id, email: user.email, createdAt: user.createdAt, updatedAt: user.updatedAt, image: user.image,};
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ success: true, token: token, status: 'Login Successful!', user: userInfo });
+					});
+				}
+			})(req, res, next);
+		})
+});
+
 
 
 module.exports = router;
